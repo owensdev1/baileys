858c0dc88b2822c18d1a8c47499a73f8b9eced62
kenzz <kenzshop1122@gmail.com>
2025-10-17 07:46:53 +0700

    Update Node.js version requirement to 23

--- engine-requirements.js
+++ engine-requirements.js
@@ -1,10 +1,10 @@
 const major = parseInt(process.versions.node.split('.')[0], 10);
 
-if (major < 20) {
+if (major < 23) {
   console.error(
-    `\n❌ Paket ini memerlukan Node.js versi 20 atau lebih untuk berjalan dengan stabil.\n` +
+    `\n❌ Paket ini memerlukan Node.js versi 23 atau lebih untuk berjalan dengan stabil.\n` +
     ` Anda menggunakan Node.js ${process.versions.node}.\n` +
-    ` Silakan perbarui ke Node.js versi 20 atau lebih untuk melanjutkan.\n`
+    ` Silakan perbarui ke Node.js versi 23 atau lebih untuk melanjutkan.\n`
   );
   process.exit(1);
 }
